d4434f5713eb210ab3ab52b42f5c76f41d75ea0d
micsand <124559135+micsand@users.noreply.github.com>
2023-03-14 00:55:55 +0800

    Create IP.js

---
+++ 配置文件合集/IP.js
@@ -0,0 +1,15 @@
+#我配置中有两种，一种是KOPXIAO的，一个是Orz3的，根据需要我用了后者的，后来发现前者还能显示ip地址，后者吧能，所以我决定在后者基础上增加显示ip的功能。
+
+if ($response.statusCode != 200) {
+  $done(Null);
+}
+
+var body = $response.body;
+var obj = JSON.parse(body);
+var title = obj['country'];
+var subtitle = obj['city'] + ' ' + obj['isp'];
+var ip = obj['query'];
+var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' +'IP:'+ obj['query'] +"数据中心" + ":" + obj['org'];
+
+
+$done({title, subtitle, ip, description});
